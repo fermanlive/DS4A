2b49fe224b328887234c2c8fa61e06eef0dfcb54
fermanlive <fernando.link32@gmail.com>
2022-05-10 21:03:49 -0500

    update scripts

--- OCR/image2string.py
+++ OCR/image2string.py
@@ -1,13 +1,20 @@
 import pytesseract
-
+import os
 from PIL import Image
+import json
 
 def image2string(routefile:str):
     img = Image.open(routefile) # Abre la imagen con pillow
     img.load()
     text = pytesseract.image_to_string(img, lang='spa') # Extrae el texto de la imagen
-    print(text)
+    return text
 
-if __name__ == "__main__":
-    image2string('output_images/page0.jpg')
-    
+def search_image(directory_str:str):
+    directory = os.fsencode(directory_str)
+    list_text = {}
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if filename.endswith("jpg"): 
+            route_file_image = image2string(directory_str+'/'+filename)
+            list_text[filename[:-4]]=route_file_image
+    return list_text
