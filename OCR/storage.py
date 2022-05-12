34146b0e9281a7d728d4a6ff3c1c1a707ddb4832
fermanlive <fernando.link32@gmail.com>
2022-05-11 19:51:31 -0500

    update bucket

--- OCR/storage.py
+++ OCR/storage.py
@@ -3,7 +3,7 @@ from google.cloud import storage
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'credentials/ds4a-349915-f8bcca5971e3.json'
 client = storage.Client()
-bucket = client.get_bucket('rc-ana-zona-cruda-gcs')
+bucket = client.get_bucket('ds4a-tierras')
 
 def upload_to_bucket(route:str, extension:str) -> None:
     bucket.blob(route).upload_from_file(route+extension)
